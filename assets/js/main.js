0ba2c86d4ca57137f127e9dc9b0a83d4a1c2271e
rositadiazgalaz <rositadiazgalaz@gmail.com>
2017-05-10 06:23:53 -0300

    se logro imprimir miembros

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
 
-function MiembrosSquad(id,nombre,apellido,edad,hobbies){
+function MiembrosSquad(id,nombre,edad,hobbies){
 	this.id=id;
 	this.nombre=nombre;
-	this.apellido=apellido;
 	this.edad=edad;
 	this.hobbies=hobbies;
+	this.comentarios =[];
 }
-var comentario=[];
-function Comentario(idGente, comentarios,likes){
+
+function Comentario(idGente, comentarios){
 	this.idGente=idGente;
 	this.comentarios=comentarios;
-	this.likes=likes;
+	this.likes=0;
 }
 
 var squad=[];
 //instancia de cada amiga
-var laura= new MiembrosSquad(1,"Laura","Peláez", 22,["cinéfila"]);
-var orieta= new MiembrosSquad(2,"Orieta","Toro", 27,["dormir","leer"]);
-var coni= new MiembrosSquad(3,"Constanza","Pecori", 26,["tejer","comer","dormir"]);
-var rangi = new MiembrosSquad(4,"Rangi","Becerra", 32,["viajar"]);
-var cate = new MiembrosSquad(5,"Caterina","Da Silva", 26,["juegos online"]);
-var rosa = new MiembrosSquad(6,"Rosa","Diaz", 32,["pensar en la inmortalidad del cangrejo"]);
-var gene = new MiembrosSquad(7,"Genesis","Narváez", 27,["pasear en bici"]);
-var cami = new MiembrosSquad(8,"Camila","González", 27,["ver netflix","jugar con mascotas","degustar cerveza"]);
+var laura= new MiembrosSquad("laura","Laura Peláez", 22,["cinéfila"]);
+var orieta= new MiembrosSquad("orieta","Orieta Toro", 27,["dormir","leer"]);
+var coni= new MiembrosSquad("coni","Constanza Pecori", 26,["tejer","comer","dormir"]);
+var rangi = new MiembrosSquad("rangi","Rangi Becerra", 32,["viajar"]);
+var cate = new MiembrosSquad("cate","Caterina Da Silva", 26,["juegos online"]);
+var rosa = new MiembrosSquad("rosa","Rosa Diaz", 32,["pensar en la inmortalidad del cangrejo"]);
+var gene = new MiembrosSquad("gene","Genesis Narváez", 27,["pasear en bici"]);
+var cami = new MiembrosSquad("cami","Camila González", 27,["ver netflix","jugar con mascotas","degustar cerveza"]);
 squad.push(laura,orieta,coni,rangi,cate,rosa,gene,cami);
 
 function imprimir(gente){
@@ -30,7 +30,7 @@ function imprimir(gente){
 	var texto= "";
 	squad.forEach(function(gente){
 		texto +=
-		"<div>" + "<img src=img/" + gente.id + ".png>" + "</div>" +"<div>" + gente.nombre + " " + gente.apellido + " " + gente.edad +"</div>"
+		"<div>" + "<img class='foto' src=assets/imgenes/" + gente.id + ".jpg>" + "</div>" +"<div>" + gente.nombre + " " + gente.edad +"</div>"
 	
 		gente.hobbies.forEach(function(hobbies){
 
